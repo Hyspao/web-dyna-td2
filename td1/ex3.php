615d6d15f2126ac3b42d1bdf83266eeb6739a503
Hyspao <antoinezyd@gmail.com>
2020-03-25 16:57:53 +0100

    Ajout ex2 et ex3

--- td1/ex3.php
+++ td1/ex3.php
@@ -1,21 +1,28 @@
-<h1>Exercice 2</h1>
-<a href="?message=Rouge&size=12&couleur=red">Rouge en 12</a><br>
-<a href="?message=Vert&size=14&couleur=green">Vert en 14</a><br>
-<a href="?message=Bleu&size=16&couleur=blue">Bleu en 16</a><br>
-
+<h1>Exercice 3</h1>
 <?php
-$size=$_GET["size"]??"10";
-$color=$_GET["color"]??"black";
-$message=$_GET["message"]??"Bonsoir";
+$size=$_POST["size"]??"10";
+$color=$_POST["color"]??"black";
+$message=$_POST["message"]??"Bonsoir";
+$update=$_POST['update']??null;
+
+if ($update=="+") {
+    $size=$size+1;
+} elseif ($update=="-"){
+    $size=$size-1;
+}
+
 echo "<div style='font-size: {$size}px; color: {$color};'> Message de taille {$size}px en {$message}</div>";
+
 ?>
 
-<form method="GET">
+<form method="POST">
     <label for="message">Modifier le message : </label>
-    <input type="text" value="" name="message" id="message">
+    <textarea name="message" id="message"><?=$message?></textarea>
     <label for="size">Modifier la taille du texte : </label>
     <input type="number" value="" name="size" id="size">
     <label for="color">Modifier la couleur du texte : </label>
     <input type="color" value="" name="color" id="color">
     <input type="submit" value="Valider">
+    <input type="submit" name="update" value="+">
+    <input type="submit" name="update" value="-">
 </form>
