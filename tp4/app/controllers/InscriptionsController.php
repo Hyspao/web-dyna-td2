ba2664384c90dd6524c9e57f8ab9a4e62b2670a8
Antoine Zydlowski <antoinezyd@gmail.com>
2020-05-13 09:59:11 +0200

    4e commit du tp4

--- tp4/app/controllers/InscriptionsController.php
+++ tp4/app/controllers/InscriptionsController.php
@@ -1,8 +1,15 @@
 <?php
 namespace controllers;
+
+use Ubiquity\orm\DAO;
+use Ubiquity\utils\http\URequest;
+use Ubiquity\utils\http\UResponse;
+use Ubiquity\utils\http\USession;
+
  /**
  * Controller InscriptionsController
  **/
+ 
 class InscriptionsController extends ControllerBase{
 
 	public function index(){
