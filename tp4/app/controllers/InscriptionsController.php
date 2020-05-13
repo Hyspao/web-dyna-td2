a0a5a98efaf5b97532966048f1a05ca7d34a0ddf
Antoine Zydlowski <antoinezyd@gmail.com>
2020-05-13 10:40:15 +0200

    5e commit du tp4

--- tp4/app/controllers/InscriptionsController.php
+++ tp4/app/controllers/InscriptionsController.php
@@ -1,6 +1,7 @@
 <?php
 namespace controllers;
 
+use models\Inscription;
 use Ubiquity\orm\DAO;
 use Ubiquity\utils\http\URequest;
 use Ubiquity\utils\http\UResponse;
@@ -9,7 +10,7 @@ use Ubiquity\utils\http\USession;
  /**
  * Controller InscriptionsController
  **/
- 
+
 class InscriptionsController extends ControllerBase{
 
 	public function index(){
@@ -17,7 +18,20 @@ class InscriptionsController extends ControllerBase{
 	}
 
 	public function inscription(){
+    $this->loadView('InscriptionsController/inscription.html');
+  }
 
-        $this->loadView('InscriptionsController/inscription.html');
-    }
+	public function do(){
+		$var = new Inscription();
+        $var->setNom($_POST["nomoupseudo"]);
+        $var->setEmail($_POST["adressemail"]);
+				if(DAO::insert($var)){
+		          echo "Votre inscription à fonctionnée, vous allez être redirigé";
+		            UResponse::header("location" ,"/inscriptionsController/index.html");
+		        }
+		        else{
+		            echo "Votre inscription n'a pas fonctionnée, vous allez être redirigé à la page d'inscription";
+								UResponse::header("location" ,"/inscriptionsController/inscription.html");
+		        }
+	}
 }
