f61fe16f6d370ec06120a5071bd30c2f9868a7c1
Antoine Zydlowski <antoinezyd@gmail.com>
2020-05-13 08:40:10 +0200

    1er commit du tp4

---
+++ tp4/app/cache/git/settings.cache.php
@@ -0,0 +1,2 @@
+<?php
+return array("name"=>"web-dyna-td2","remoteUrl"=>"https://github.com/Hyspao/web-dyna-td2","baseFolder"=>"C:\\xampp\\htdocs\\web-dyna-td2","user"=>"Hyspao","password"=>"e7c830033e");
