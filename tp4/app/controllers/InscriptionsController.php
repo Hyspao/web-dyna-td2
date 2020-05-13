35e879a5d919565a11c01ae388c9de995245dbac
Antoine Zydlowski <antoinezyd@gmail.com>
2020-05-13 09:32:19 +0200

    3e commit du tp4

---
+++ tp4/app/controllers/InscriptionsController.php
@@ -0,0 +1,16 @@
+<?php
+namespace controllers;
+ /**
+ * Controller InscriptionsController
+ **/
+class InscriptionsController extends ControllerBase{
+
+	public function index(){
+		$this->loadView("InscriptionsController/index.html");
+	}
+
+	public function inscription(){
+
+        $this->loadView('InscriptionsController/inscription.html');
+    }
+}
